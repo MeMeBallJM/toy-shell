ca0bd12c04e29a38822284e80f17e6cc7051a826
Joshua Morton <morton.l.joshua@gmail.com>
2026-04-30 16:30:08 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
-#[allow(unused_imports)]
-use std::io::{self, Write};
+use std::io;
+use std::io::Write;
 
 fn main() {
     print!("$ ");
-    io::stdout().flush().unwrap();
+    io::stdout().flush().expect("Unable to flush prompt");
+
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+
+    if let Err(error) = stdin.read_line(&mut buffer) {
+        println!("Unable to read line from stdin: {}", error);
+        return;
+    }
+
+    let input = &buffer[0..buffer.len() - 1];
+
+    println!("{}: {} not found", input, input);
 }
